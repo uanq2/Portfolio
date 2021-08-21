fdd045aa5878d564d8284e26647a9964d7d29ea9
Uriel Nunez <uanq22@hotmail.com>
2021-08-21 00:23:25 -0500

    Created and added content to components

---
+++ src/components/Cards.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import CardItem from './CardItem';
+import './Cards.css';
+
+function Cards() {
+    return (
+        <div className='cards'>
+            <h1>Web Development</h1>
+            <div className="cards_container">
+                <div className="cards_wrapper">
+                    <ul className="cards_items">
+                        <CardItem
+                            src='images/Vibe_Check.png'
+                            text='Music Generator App'
+                            Link='https://project-one-group-three.github.io/vibe-check/' target='_blank'
+                        />
+                        <CardItem
+                            src='images/WeatherDash.png'
+                            text='Weather Forecast in any city App'
+                            href='https://project-one-group-three.github.io/vibe-check/' target='_blank'
+                        />
+                        <CardItem
+                            src='images/Trivia_Game.png'
+                            text='Trivia app to test your knowledge on Soccer World Cup'
+                            href='https://project-one-group-three.github.io/vibe-check/' target='_blank'
+                        />
+                    </ul>
+                </div>
+            </div>
+            <h1>Graphic Design</h1>
+            <div className="cards_container">
+                <div className="cards_wrapper">
+                    <ul className="cards_items">
+                        <CardItem
+                            src='images/RaffaeloMockUp.jpg'
+                            text='Hotel Brochure'
+                            Link='https://project-one-group-three.github.io/vibe-check/' target='_blank'
+                        />
+                        <CardItem
+                            src='images/Postcards.jpg'
+                            text='Postcards 5 Senses Design'
+                            href='https://project-one-group-three.github.io/vibe-check/' target='_blank'
+                        />
+                        <CardItem
+                            src='images/Invitation.jpg'
+                            text='Private Event Invitation to showcase new establishment'
+                            href='https://project-one-group-three.github.io/vibe-check/' target='_blank'
+                        />
+                    </ul>
+                </div>
+            </div>
+        </div >
+    )
+}
+
+export default Cards
--- src/components/FrontSection.js
+++ src/components/FrontSection.js
@@ -5,10 +5,10 @@ import './FrontSection.css';
 function FrontSection() {
     return (
         <div className='front-container'>
-            <img src="/public/images/Avatar.png" alt="avatar"></img>
+            <img src="/public/images/AdobeStock_285705907.jpeg" alt="Coding" />
             <h1>THANK YOU FOR VISITING!</h1>
             <p>Let's connect to create amazing projects</p>
-        </div>
+        </div >
     )
 }
 
